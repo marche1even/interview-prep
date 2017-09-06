1c6327af157b776743779b8c28ef7774726ecc5d
Frances <francesrosetan@gmail.com>
2017-09-06 13:51:27 -0700

    added a folder

---
+++ ch1/ch_1_2.java
@@ -0,0 +1,15 @@
+// Check Permutation
+// Given two strings, write a method to decide if one
+// is a permutation of the other
+
+public class ch_1_2 {
+
+    public static boolean isPermutation(String a, String b) {
+
+        return true;
+    }
+    
+    public static void main(String args[]) {
+
+    }
+}
